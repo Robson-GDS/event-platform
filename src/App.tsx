1a42235b37e2219010cd7e66b98e62677d526c3a
unknown <robson.080490@gmail.com>
2022-06-22 06:54:14 -0300

    criando estrutura dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
+import { Event } from "./pages/Event"
 
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY)
-
-  console.log(data)
-
-  return (
-    <h1 className="text-5xl font-bold text-violet-500">Teste</h1>
+  return (  
+    <div>
+      <Event />
+    </div>
   )
 }
 
