2b6bce4da87c5ebe7b4a95996abf0f1aecd75bc8
unknown <robson.080490@gmail.com>
2022-06-24 06:21:25 -0300

    finalizando estrutura da pagina subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -16,7 +16,27 @@ export function Subscribe() {
         </div>
 
         <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">Inscreva-se gratuitament</strong>
+          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+
+          <form action="" className="flex flex-col gap-2 w-full">
+            <input 
+              className="bg-gray-900 rounded px-5 h-14"
+              type="text"
+              placeholder="Seu nome completo" 
+            />
+            <input 
+              className="bg-gray-900 rounded px-5 h-14"
+              type="email"
+              placeholder="Digite seu e-mail" 
+            />
+
+            <button 
+              type="submit"
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+            >
+              Garantir minha vaga
+            </button>
+          </form>
         </div>
       </div>
       
